51416c869838d10e1140dd74c9f734c779f97727
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-04 15:38:50 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/navbar.php");?>
+<?php include("includes/headertop.php");?>
+
+<?php include("includes/slider.php");?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Hello</h1>
+                <a class="btn btn-primary" href="login.php" role="button">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php"); ?>
