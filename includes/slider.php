32b2b6a3989bc23247a247267079d20f4d6a7f75
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-04 16:29:45 +0700

    update

--- includes/slider.php
+++ includes/slider.php
@@ -11,23 +11,23 @@
         <div class="row">
             <div class="col-md-12">
                 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
+                    <!-- <div class="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    </div>
+                    </div> -->
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="images/slider1.jpg" class="d-block w-100" alt="...">
                             <div class="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
+                                <!-- <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p> -->
                             </div>
                         </div>
                         <div class="carousel-item">
                             <img src="images/slider2.png" class="d-block w-100" alt="...">
                             <div class="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
+                                <!-- <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p> -->
                             </div>
                         </div>
                     </div>
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php include("includes/header.php"); ?>
-<?php include("includes/navbar.php");?>
-<?php include("includes/headertop.php");?>
+<?php include("includes/navbar.php"); ?>
+<?php include("includes/headertop.php"); ?>
 
-<?php include("includes/slider.php");?>
+<?php include("includes/slider.php"); ?>
 
 <div class="py-5">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1>Hello</h1>
-                <a class="btn btn-primary" href="login.php" role="button">Login</a>
+                <h2 class="text-center"><span class="text-danger">วิทยาลัยเทคนิคตราด</span> สังกัดสำนักงานคณะกรรมการการอาชีวศึกษา</h2>
+                <h4 class="text-center text-info mt-2">แนวคิดการจัดการศึกษา</h4>
             </div>
         </div>
     </div>
