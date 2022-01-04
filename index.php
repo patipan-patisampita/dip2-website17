dfc9b7da31edaa12017b99ba71719d26a7b65389
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-04 16:44:24 +0700

    update

--- index.php
+++ index.php
@@ -4,15 +4,6 @@
 
 <?php include("includes/slider.php"); ?>
 
-<div class="py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h2 class="text-center"><span class="text-danger">วิทยาลัยเทคนิคตราด</span> สังกัดสำนักงานคณะกรรมการการอาชีวศึกษา</h2>
-                <h4 class="text-center text-info mt-2">แนวคิดการจัดการศึกษา</h4>
-            </div>
-        </div>
-    </div>
-</div>
 
+<?php include("includes/section.php");?>
 <?php include("includes/footer.php"); ?>
