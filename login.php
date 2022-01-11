38574b787ffcba30a5176a3c7578544b55829822
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-11 16:29:05 +0700

    add

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <?php include("includes/header.php"); ?>
 <?php include("includes/navbar.php"); ?>
 
@@ -13,6 +15,9 @@
 
     <div class="row justify-content-center">
         <div class="col-md-7">
+
+        <?php include("message.php"); ?>
+
             <div class="card shadow">
 
                 <div class="card-header">
