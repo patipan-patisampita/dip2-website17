600671063d3fb0399aef8563730ecbab46007204
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-25 17:30:48 +0700

    update

--- admin/news1.php
+++ admin/news1.php
@@ -2,6 +2,7 @@
 <?php include("includes/header.php"); ?>
 <?php include("includes/sidebar.php"); ?>
 <?php include("includes/topbar.php"); ?>
+<<?php include("config/dbcon.php"); ?>
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
@@ -21,7 +22,18 @@
             </div>
 
             <div class="card-body">
+                <?php 
+                    if(isset($_SESSION['success']) && $_SESSION['success'] !=''){
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                    }
+                ?>
                 <div class="table-responsive">
+                    <?php 
+                        $query = "SELECT * FROM hotnews";
+                        $res = mysqli_query($con,$query);
+                    ?>
+
                     <table class="table table-bordered border-primary">
                         <thead>
                             <tr>
@@ -34,26 +46,34 @@
                             </tr>
                         </thead>
                         <tbody>
+                            <?php 
+                                $i = 1;
+                                while($row = mysqli_fetch_array($res)){
+                             ?>
                             <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+                                <th scope="row"><?php echo $i; ?></th>
+                                <td><?php echo $row['head']; ?></php></td>
+                                <td><?php echo $row['detail']; ?></td>
+                                <td><?php echo $row['date']; ?></td>
                                 <td>
                                     <form action="news_edit.php" method="post">
-                                        <input type="hidden" name="news_edit_id">
-                                        <button type="submit" name="news_editbtn" class="btn btn-success">แก้ไขข่าวสาร</button>
+                                        <input type="hidden" name="news_edit_id" value="<?php echo $row['id'] ?>">
+                                        <button type="submit" name="news_editbtn" class="btn btn-success">แก้ไข</button>
                                     </form>
                                 </td>
                                 <td>
                                     <form action="news_code.php" method="post">
-                                        <input type="hidden" name="delete_image">
-                                        <input type="hidden" name="delete_doc">
-                                        <input type="hidden" name="delete_id">
-                                        <button type="submit" name="news_deletebtn" class="btn btn-danger">ลบข่าวสาร</button>
+                                        <input type="hidden" name="delete_image"  value="<?php echo $row['namepic'] ?>">
+                                        <input type="hidden" name="delete_doc"  value="<?php echo $row['namedoc'] ?>">
+                                        <input type="hidden" name="delete_id"  value="<?php echo $row['id'] ?>">
+                                        <button type="submit" name="news_deletebtn" class="btn btn-danger">ลบ</button>
                                     </form>
                                 </td>
                             </tr>
+                            <?php 
+                            $i++;
+                             }
+                            ?>
                         </tbody>
                     </table>
                 </div>
