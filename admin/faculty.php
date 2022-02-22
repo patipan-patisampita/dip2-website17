65b38bed76b10db6e2c874d2b95af752a78597af
patipan-patisampita <patipan.patisampita@gmail.com>
2022-02-22 13:43:46 +0700

    update

---
+++ admin/faculty.php
@@ -0,0 +1,11 @@
+<?php include("security.php"); ?>
+<?php include("includes/header.php"); ?>
+<?php include("includes/sidebar.php"); ?>
+<?php include("includes/topbar.php"); ?>
+<?php include("config/dbcon.php"); ?>
+
+Hello faculty
+
+<?php include("includes/footer.php") ?>
+<?php include("includes/logoutmodal.php") ?>
+<?php include("includes/scripts.php") ?>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -36,8 +36,8 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                        <a class="collapse-item" href="abouts.php">เกี่ยวกับ</a>
+                        <a class="collapse-item" href="faculty.php">คณะผู้บริหาร</a>
                     </div>
                 </div>
             </li>
